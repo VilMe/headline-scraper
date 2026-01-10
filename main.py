c590b177109ebc85aa974c61a81fb5c6724ab7f2
VilMe <vilma.medrano00@gmail.com>
2026-01-09 21:57:22 -0600

    defin get soup funntion complete

--- main.py
+++ main.py
@@ -2,4 +2,11 @@ from bs4 import BeautifulSoup
 import requests
 
 def get_soup() -> BeautifulSoup:
-    headers: dict = {'User-Agent': }
+    headers: dict = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36'}
+    request = requests.get('https://ww.bbc.com/news', headers=headers)
+    html: bytes = request.content
+
+    # Create soup
+    soup = BeautifulSoup(html, 'html.parser')
+    return soup
+
